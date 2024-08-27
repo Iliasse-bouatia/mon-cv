193ca2a67d31ba4815169323bfe85956762eef79
greennoi <iliassebouatia506@gmail.com>
2024-08-27 12:57:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,23 +19,11 @@ export default function YourComponent() {
 
   return (
     <div>
-      <header className="navbar">
-        <div className="container">
-          <a href="/" className="logo">Logo</a>
-          <div className="menu-toggle">
-            <div className="bar"></div>
-            <div className="bar"></div>
-            <div className="bar"></div>
-          </div>
-          <ul className="nav-links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div>
-      </header>
+      {/* Your JSX structure here */}
+      <button className="menu-toggle">Menu</button>
+      <nav className="nav-links">
+        {/* Nav links here */}
+      </nav>
     </div>
   );
 }
